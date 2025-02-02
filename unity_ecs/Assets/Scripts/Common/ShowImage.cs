a948cc856b4071c76686011d9a1de2758c666cb5
JUNMIZUNO_PAV\gvist <create.gamescene@gmail.com>
2025-02-02 16:29:09 +0900

    イメージ表示用のスクリプトを作成

---
+++ unity_ecs/Assets/Scripts/Common/ShowImage.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using Cysharp.Threading.Tasks;
+using System;
+
+public class ShowImage : MonoBehaviour
+{
+    [SerializeField]
+    private Button button;
+
+    [SerializeField]
+    private Image image;
+
+    [SerializeField]
+    private RawImage rawImage;
+
+    private bool isBuiltinImage = true;
+
+    async void Start()
+    {
+        if (image == null)
+        {
+            isBuiltinImage = false;
+        }
+
+        await UniTask.Delay(TimeSpan.FromSeconds(3f));
+
+        Debug.Log($"DelayTest:{0}");
+    }
+
+    void Update()
+    {
+        
+    }
+}
